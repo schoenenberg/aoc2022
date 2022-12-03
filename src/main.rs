748d237ef5fa73722229da65c8fe5cce9becc1a1
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2022-12-03 06:57:43 +0100

    Day03 added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod day01;
 mod day02;
+mod day03;
 
 fn main()  -> Result<(), Box<dyn std::error::Error>> {
     println!("Day01: {:#?}", day01::solve("input/day01.txt")?);
     println!("Day02: {:#?}", day02::solve("input/day02.txt")?);
+    println!("Day03: {:#?}", day03::solve("input/day03.txt")?);
 
     Ok(())
 }
