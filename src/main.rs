129759351e367e7e963b5634eb2c00874db08fe2
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2022-12-03 07:41:36 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod day03;
 
 macro_rules! print_day {
     ($day:expr, $fun:path) => {
-        println!("Day{:0>2}: {:?}", $day, $fun(format!("input/day{:0>2}.txt", $day).as_str())?);
+        println!(
+            "Day{:0>2}: {:?}",
+            $day,
+            $fun(format!("input/day{:0>2}.txt", $day).as_str())?
+        );
     };
 }
 
-fn main()  -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     print_day!(1, day01::solve);
     print_day!(2, day02::solve);
     print_day!(3, day03::solve);
