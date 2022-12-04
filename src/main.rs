c9b343c056a2f1ad2676c95beae586b1fa4027fc
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2022-12-04 06:41:53 +0100

    Day04 added

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod macros;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     print_day!(1, day01);
     print_day!(2, day02);
     print_day!(3, day03);
+    print_day!(4, day04);
 
     Ok(())
 }
